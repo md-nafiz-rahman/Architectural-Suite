df37f049caf9b41a5c93bdaeacbe5d2e63d0b3e2
mnr10 <mnr10@student.le.ac.uk>
2024-01-25 19:51:02 +0000

    Added Main Menu interface
    Added main menu UI elements; placeholders currently in place, full implementation pending.

---
+++ Eco Design Fire Safety/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void StartNewDesign()
+    {
+
+        SceneManager.LoadScene("Environment");
+    }
+
+    public void LoadDesign()
+    {
+        Debug.Log("Load Design functionality not implemented yet.");
+    }
+
+    public void Options()
+    {
+        Debug.Log("Options functionality not implemented yet.");
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        Debug.Log("ExitGame method called - Exiting game in editor.");
+#else
+        Application.Quit();
+#endif
+    }
+}
