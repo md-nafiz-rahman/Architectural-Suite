5049bd69190d4b4148a1e777565d156aeb36ac0d
mnr10 <mnr10@student.le.ac.uk>
2024-02-23 21:14:06 +0000

    Added Pause Menu
    Added pause menu screen and script.

---
+++ Eco Design Fire Safety/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenuUI;
+
+    void Start()
+    {
+        pauseMenuUI.SetActive(false);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (pauseMenuUI.activeSelf)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+
+    public void Pause()
+    {
+        Debug.Log("Pause called. pauseMenuUI active state: " + pauseMenuUI.activeSelf);
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        Cursor.lockState = CursorLockMode.None; 
+        Cursor.visible = true;
+    }
+
+    public void Resume()
+    {
+        Debug.Log("Resume called. pauseMenuUI active state: " + pauseMenuUI.activeSelf);
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f; 
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void SaveGame()
+    {
+        Debug.Log("SaveGame not implemented yet.");
+    }
+}
