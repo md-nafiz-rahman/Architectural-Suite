7b4fafce20da48d7f7965d520650c05feea971c2
mnr10 <mnr10@student.le.ac.uk>
2024-04-17 23:29:27 +0100

    Added new script to prevent necessary GameObjects from destroying when restarting the game

---
+++ Eco Design Fire Safety/Assets/Scripts/DontDestroy.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroy : MonoBehaviour
+{
+    [HideInInspector]
+    public string ObjectID;
+    private void Awake()
+    {
+        ObjectID = name + transform.position.ToString() + transform.eulerAngles.ToString();
+    }
+
+    private void Start()
+    {
+        for (int i = 0; i < Object.FindObjectsOfType<DontDestroy>().Length; i++)
+        {
+
+            if (Object.FindObjectsOfType<DontDestroy>()[i] != this)
+            {
+                if (Object.FindObjectsOfType<DontDestroy>()[i].ObjectID == ObjectID)
+                {
+                    Destroy(gameObject);
+                }
+            }
+
+
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+
+    }
+}
